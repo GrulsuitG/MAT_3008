5f7aabbc5129dd9ceb85219b16154f98603bfd34
2016025025 <2016025025@hanyang.ac.kr>
2021-09-28 18:25:14 +0900

    HW4
    Former-commit-id: a32edd97f10c0a4c47e862a365400a5521ab6cea [formerly d5c1155b610d07bdc48186cd42e75c6b7cf0e52d]
    Former-commit-id: ef6efc846316620fedf323fbd8565adda2cdee18

---
+++ HW4/HW4/linerinterpolation.c
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+/* Driver for routine rtflsp */
+
+#include <stdio.h>
+#include <math.h>
+#define NRANSI
+#include "nr.h"
+#include "nrutil.h"
+
+#define N 100
+#define NBMAX 20
+#define X1 0.0
+#define X2 400.0
+
+static float fx(float x)
+{
+	return exp(-0.005 * x) * cos(sqrt(2000 - (0.01 * x * x)) * 0.05) - 0.01;
+}
+
+
+
+int linearinterpolation(void)
+{
+	int i, nb = NBMAX;
+	float xacc, root, * xb1, * xb2;
+
+	xb1 = vector(1, NBMAX);
+	xb2 = vector(1, NBMAX);
+	zbrak(fx, X1, X2, N, xb1, xb2, &nb);
+	printf("\nliner interpolation:\n");
+
+	for (i = 1; i <= nb; i++) {
+		xacc = (1.0e-4);
+		printf("accuracy 10^-4\n");
+		root = rtflsp(fx, xb1[i], xb2[i], xacc);
+		printf("root %3d %14.6f %14.6f\n", i, root, fx(root));
+	}
+	for (i = 1; i <= nb; i++) {
+		xacc = (1.0e-6);
+		printf("accuracy 10^-6\n");
+		root = rtflsp(fx, xb1[i], xb2[i], xacc);
+		printf("root %3d %14.6f %14.6f\n", i, root, fx(root));
+	}
+	free_vector(xb2, 1, NBMAX);
+	free_vector(xb1, 1, NBMAX);
+	return 0;
+}
+
+#undef NRANSI
