8e2ff71cdfdf57c3d308d187f8c9a80a1228fa4f
2016025025 <2016025025@hanyang.ac.kr>
2021-11-01 23:15:55 +0900

    HW6
    Former-commit-id: c9f9877816449b7bb9e89d71664501b41e2cafea

---
+++ HW6/HW6/HW6\gasdev.c
@@ -0,0 +1,28 @@
+
+#include <math.h>
+
+float gasdev(long* idum, float mean, float stdev)
+{
+	float ran1(long* idum);
+	static int iset = 0;
+	static float gset;
+	float fac, rsq, v1, v2;
+
+	if (*idum < 0) iset = 0;
+	if (iset == 0) {
+		do {
+			v1 = 2 * ran1(idum) - 1;
+			v2 = 2 * ran1(idum) - 1;
+			rsq = v1 * v1 + v2 * v2;
+		} while (rsq >= 1.0 || rsq == 0.0);
+		fac = sqrt(-2.0 * log(rsq) / rsq);
+		
+		gset = v1 * fac;
+		gset = (stdev*gset)+mean;
+		return gset;
+	}
+	else {
+		iset = 0;
+		return gset;
+	}
+}
