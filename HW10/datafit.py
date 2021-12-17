cefafed52045e90d796eac30b48967e18b19af64
2016025025 <2016025025@hanyang.ac.kr>
2021-12-17 15:49:41 +0900

    HW10 update
    Former-commit-id: 254da1e4d88e106013041aa99b51733efaf39367 [formerly 2d64331bd2ef0a95fca0ccedf1a4a6b3f2d32e3a]
    Former-commit-id: eb035edce004d947c0c65ea6b4b1613a8878fe29

--- HW10/datafit.py
+++ HW10/datafit.py
@@ -1,4 +1,5 @@
 import numpy as np
+import csv
 
 def func_x(x, y, a):
     return numerator_x(x,y,a)/denominator(x,y,a)
@@ -16,78 +17,66 @@ def denominator(x,y,a):
     return a[2][0]*x+a[2][1]*y+1
 
 def main():
-    for i in range(1, 4):
-        filename = "fitdata" + str(i) + ".dat"
-        f = open(filename, 'r')
-        lines = f.readlines()
-        x_arr = np.array([])
-        y_arr = np.array([])
-        xp_arr = np.array([])
-        yp_arr = np.array([])
-        for l in lines :
-            num = l.split();
-            x = float(num[0])
-            y = float(num[1])
-            xp = float(num[2])
-            yp = float(num[3])
-        
-            x_arr = np.append(x_arr, x)
-            y_arr = np.append(y_arr, y)
-            xp_arr = np.append(xp_arr, xp)
-            yp_arr = np.append(yp_arr, yp)
+    filename = "data.csv"
+    f = open(filename, 'r')
+    lines = csv.reader(f)
+    x_arr = np.array([])
+    y_arr = np.array([])
+    xp_arr = np.array([])
+    yp_arr = np.array([])
+    for num in lines :
+        if num[0] == 'N':
+            continue
+        x = float(num[1])
+        y = float(num[2])
+        xp = float(num[3])
+        yp = float(num[4])
 
-        
-        a_cur = np.random.randn(3,3)
-        a_cur[2][2] = .0
-        chi_x,chi_y = chi_square(x_arr, y_arr, xp_arr, yp_arr, a_cur)
-        lamdax = 0.001
-        lamday = 0.001
+        x_arr = np.append(x_arr, x)
+        y_arr = np.append(y_arr, y)
+        xp_arr = np.append(xp_arr, xp)
+        yp_arr = np.append(yp_arr, yp)
+
+
+    a_cur = np.random.randn(3,3)
+    a_cur[2][2] = .0
+    chi = chi_square(x_arr, y_arr, xp_arr, yp_arr, a_cur)
+    lamda = 0.001
+    while True:
+        gradient, hessian = get_gradient_hessian(x_arr, y_arr, xp_arr, yp_arr, a_cur)
+
+        modified_hessian = hessian + np.eye(8,8)*lamda 
         while True:
-            g_x, g_y, h_x, h_y = get_gradient_hessian(x_arr, y_arr, xp_arr, yp_arr, a_cur)
-            mh_x = h_x + np.eye(5,5)*lamdax
-            
-            while True:
-                if np.linalg.det(mh_x) != 0:
-                    mh_x_inv = np.linalg.inv(mh_x)
-                    break
-                else :
-                    mh_x += 0.01
-
-            mh_y = h_y + np.eye(5,5)*lamday
-            while True:
-                if np.linalg.det(mh_y) != 0:
-                    mh_y_inv = np.linalg.inv(mh_y)
-                    break
-                else :
-                    mh_y += 0.01
-            da_x = np.dot(mh_x_inv,g_x.T)
-            da_y = np.dot(mh_y_inv,g_y.T)
-            
-            da = np.array([ [da_x[0],da_x[1],da_x[2]],
-                            [da_y[0],da_y[1],da_y[2]],
-                            [(da_x[3]+da_y[3])/2, (da_x[4]+da_y[4])/2, 0]])
-            m = da.mean()
-            if abs(m) < 0.001 :
-                break;
-
-            a_next = da + a_cur
-
-            next_chi_x, next_chi_y = chi_square(x_arr, y_arr, xp_arr, yp_arr, a_next)
-            if next_chi_x >= chi_x:
-                lamdax *= 10
+            if np.linalg.det(modified_hessian) != 0:
+                modified_hessian_inv = np.linalg.inv(modified_hessian)
+                break
             else :
-                lamdax /= 10
-            if next_chi_y >= chi_y:
-                lamday *= 10
-            else :
-                lamday /= 10
+                modified_hessian += 0.001
 
-            a_cur = a_next
+        da = np.dot(modified_hessian, gradient.T)
+        da = np.append(da, np.array([.0]))
+        da = da.reshape(3,3)
+        
+        m = da.mean()
+        if abs(m) < 0.0001 :
+            break;
 
-        print_sol(a_cur, i)
+        a_next = da + a_cur
 
-def print_sol(a, i):
-    print("solution", i)
+
+        next_chi = chi_square(x_arr, y_arr, xp_arr, yp_arr, a_next)
+
+        if next_chi >= chi :
+            lamda *= 10
+        else :
+            lamda /= 10
+
+        a_cur = a_next
+
+    print_sol(a_cur)
+
+def print_sol(a):
+    print("solution")
 
     print("a11 :", a[0][0])
     print("a12 :", a[0][1])
@@ -99,13 +88,12 @@ def print_sol(a, i):
     print("a32 :", a[2][1])
 
     print()
-    
+
 
 def chi_square(x_arr,y_arr,xp_arr,yp_arr,a):
-    
+
     length = len(x_arr)
-    chi_x = .0
-    chi_y = .0
+    chi = .0
     for i in range(length):
         x = x_arr[i]
         y = y_arr[i]
@@ -118,17 +106,21 @@ def chi_square(x_arr,y_arr,xp_arr,yp_arr,a):
         fy = func_y(x,y,a)
         e_y = yp - fy
 
-        chi_x += e_x*e_x
-        chi_y += e_y*e_y
+        e = e_x*e_x + e_y*e_y
 
-    return chi_x, chi_y
+        chi += e 
+
+    return chi
 
 def get_gradient_hessian(x_arr, y_arr, xp_arr, yp_arr, a):
     length = len(x_arr)
-    gradient_x = np.zeros(5)
-    gradient_y = np.zeros(5)
-    hessian_x = np.full((5,5), .0)
-    hessian_y = np.full((5,5), .0)
+    #  gradient_x = np.zeros(5)
+    #  gradient_y = np.zeros(5)
+    #  hessian_x = np.full((5,5), .0)
+    #  hessian_y = np.full((5,5), .0)
+    gradient = np.zeros(8)
+    hessian = np.full((8,8), .0)
+
     for i in range(length):
         x = x_arr[i]
         y = y_arr[i]
@@ -137,12 +129,12 @@ def get_gradient_hessian(x_arr, y_arr, xp_arr, yp_arr, a):
 
         fx = func_x(x,y,a)
         e_x = xp - fx
-        
+
         fy = func_y(x,y,a)
         e_y = yp - fy
 
         deno = denominator(x,y,a)
-        
+
         dfda11 = x/deno
         dfda12 = y/deno
         dfda13 = 1/deno
@@ -154,38 +146,56 @@ def get_gradient_hessian(x_arr, y_arr, xp_arr, yp_arr, a):
         dfda21 = dfda11
         dfda22 = dfda12
         dfda23 = dfda13
-        
+
         dfxda31 = -x*fx/deno
         dfxda32 = -y*fx/deno
         dfyda31 = -x*fy/deno
         dfyda32 = -y*fy/deno
 
-        
-        tmp_gradient_x = np.array([e_x*dfda11, e_x*dfda12, e_x*a[0][2]/deno, e_x*dfxda31, e_x*dfxda32])
 
-        tmp_gradient_y = np.array([e_y*dfda21, e_y*dfda22, e_y*a[0][2]/deno, e_y*dfyda31, e_y*dfyda32])
 
-        tmp_hessian_x = np.array([[dfda11*dfda11, dfda11*dfda12, dfda11*dfda13, dfda11*dfxda31, dfda11*dfxda32],
-                                [dfda12*dfda11, dfda12*dfda12, dfda12*dfda13, dfda12*dfxda31, dfda12*dfxda32],
-                                [dfda13*dfda11, dfda13*dfda12, dfda13*dfda13, dfda13*dfxda31, dfda13*dfxda32],
-                                [dfxda31*dfda11, dfxda31*dfda12, dfxda31*dfda13, dfxda31*dfxda31, dfxda31*dfxda32],
-                                [dfxda32*dfda11, dfxda32*dfda12, dfxda32*dfda13, dfxda32*dfxda31, dfxda32*dfxda32]])
+        tmp_gradient = np.array([e_x*dfda11, e_x*dfda12, e_x*a[0][2]/deno, e_y*dfda21, e_y*dfda22, e_y*a[0][2]/deno, e_x*dfxda31+e_y*dfyda31, e_x*dfxda32+e_y*dfyda32])
 
-        tmp_hessian_y = np.array([[dfda21*dfda21, dfda21*dfda22, dfda21*dfda23, dfda21*dfyda31, dfda21*dfyda32],
-                                [dfda22*dfda21, dfda22*dfda22, dfda22*dfda23, dfda22*dfyda31, dfda22*dfyda32],
-                                [dfda23*dfda21, dfda23*dfda22, dfda23*dfda23, dfda23*dfyda31, dfda23*dfyda32],
-                                [dfyda31*dfda21, dfyda31*dfda22, dfyda31*dfda23, dfyda31*dfyda31, dfyda31*dfyda32],
-                                [dfyda32*dfda21, dfyda32*dfda22, dfyda32*dfda23, dfyda32*dfyda31, dfyda32*dfyda32]])
-        gradient_x += -2 * tmp_gradient_x
-        gradient_y += -2 * tmp_gradient_y
+        #  tmp_gradient_x = np.array([e_x*dfda11, e_x*dfda12, e_x*a[0][2]/deno, e_x*dfxda31, e_x*dfxda32])
 
-        hessian_x += tmp_hessian_x
-        hessian_y += tmp_hessian_y
-    
+        #  tmp_gradient_y = np.array([e_y*dfda21, e_y*dfda22, e_y*a[0][2]/deno, e_y*dfyda31, e_y*dfyda32])
 
-    return gradient_x, gradient_y, hessian_x, hessian_y
-       
-     
+        tmp_hessian = np.array([[dfda11*dfda11, dfda11*dfda12, dfda11*dfda13, .0, .0, .0, dfda11*dfxda31, dfda11*dfxda32],
+            [dfda12*dfda11, dfda12*dfda12, dfda12*dfda13, .0, .0, .0, dfda12*dfxda31, dfda12*dfxda32],
+            [dfda13*dfda11, dfda13*dfda12, dfda13*dfda13, .0, .0, .0, dfda13*dfxda31, dfda13*dfxda32],
+            [.0, .0, .0, dfda21*dfda21, dfda21*dfda22, dfda21*dfda23, dfda21*dfyda31, dfda21*dfyda32],
+            [.0, .0, .0, dfda22*dfda21, dfda22*dfda22, dfda22*dfda23, dfda22*dfyda31, dfda22*dfyda32],
+            [.0, .0, .0, dfda23*dfda21, dfda23*dfda22, dfda23*dfda23, dfda23*dfyda31, dfda23*dfyda32],
+            [dfxda31*dfda11, dfxda31*dfda12, dfxda31*dfda13, dfyda31*dfda21, dfyda31*dfda22, dfyda31*dfda23, 
+                dfxda31*dfxda31 + dfyda31*dfyda31, dfxda31*dfxda32 + dfyda31*dfyda32],
+            [dfxda32*dfda11, dfxda32*dfda12, dfxda32*dfda13, dfyda32*dfda21, dfyda32*dfda22, dfyda32*dfda23,
+                dfxda32*dfxda31 + dfyda32*dfyda31, dfxda32*dfxda32 + dfyda32*dfyda32]])
+
+
+        #  tmp_hessian_x = np.array([[dfda11*dfda11, dfda11*dfda12, dfda11*dfda13, dfda11*dfxda31, dfda11*dfxda32],
+        #                          [dfda12*dfda11, dfda12*dfda12, dfda12*dfda13, dfda12*dfxda31, dfda12*dfxda32],
+        #                          [dfda13*dfda11, dfda13*dfda12, dfda13*dfda13, dfda13*dfxda31, dfda13*dfxda32],
+        #                          [dfxda31*dfda11, dfxda31*dfda12, dfxda31*dfda13, dfxda31*dfxda31, dfxda31*dfxda32],
+        #                          [dfxda32*dfda11, dfxda32*dfda12, dfxda32*dfda13, dfxda32*dfxda31, dfxda32*dfxda32]])
+        #
+        #  tmp_hessian_y = np.array([[dfda21*dfda21, dfda21*dfda22, dfda21*dfda23, dfda21*dfyda31, dfda21*dfyda32],
+        #                          [dfda22*dfda21, dfda22*dfda22, dfda22*dfda23, dfda22*dfyda31, dfda22*dfyda32],
+        #                          [dfda23*dfda21, dfda23*dfda22, dfda23*dfda23, dfda23*dfyda31, dfda23*dfyda32],
+        #                          [dfyda31*dfda21, dfyda31*dfda22, dfyda31*dfda23, dfyda31*dfyda31, dfyda31*dfyda32],
+        #                          [dfyda32*dfda21, dfyda32*dfda22, dfyda32*dfda23, dfyda32*dfyda31, dfyda32*dfyda32]])
+        #  gradient_x += -2 * tmp_gradient_x
+        #  gradient_y += -2 * tmp_gradient_y
+        #
+        #  hessian_x += tmp_hessian_x
+        #  hessian_y += tmp_hessian_y
+
+        gradient += -2 * tmp_gradient
+
+        hessian += tmp_hessian
+
+    return gradient, hessian
+
+
 
 if __name__ == "__main__":
     main()
